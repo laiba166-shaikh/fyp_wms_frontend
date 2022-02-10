bc86416d26e06f25b72e8c860070ad22b65b086a
laiba166-shaikh <saashir88@gmail.com>
2022-02-11 03:59:13 +0500

    Product Inward View

--- src/redux/ProductInward/ProductInwardActions.js
+++ src/redux/ProductInward/ProductInwardActions.js
@@ -22,36 +22,19 @@ export const getAllProductInward=(page,limit)=>async (dispatch) => {
 
 export const getProductInward=(productInwardId)=> async (dispatch)=>{
     try {
-        const {data,status}=await client.get(`/product-inwards/${productInwardId}`)
+        const {data:{data},status}=await client.get(`/product-inwards/${productInwardId}`)
         console.log("api -> ",data);
         if(status === 200) {
-            return {...data.data.productInward}
+            return {
+                productInward:{...data.productInward},
+                inwardProducts:data.inwardGroups.map((inwardProd)=>({product:inwardProd.Product,quantity:inwardProd.quantity})) //Product will also contain uom
+            }
         }
     } catch (error) {
         console.log(error)
         return 0;
     }
-}
-
-// export const updateProductInward=(productInward)=>async (dispatch)=>{
-//     try {
-//         dispatch({type:PRODUCT_INWARD_START_LOADING})
-//         const {_id,...body}=product
-//         const {data}=await client.patch(`/product-inwards/${_id}`,{...body})
-//         console.log("api -> ",data);
-//         dispatch({
-//             type:UPDATE_PRODUCT_INWARD,
-//             payload:{
-//                 productInward:{...data.data.productInward}
-//             }
-//         })
-//         return 1;
-//     } catch (error) {
-//         console.log(error)
-//         dispatch({type:PRODUCT_ERROR,payload:{error:"Something went wrong"}})
-//         return 0;
-//     }   
-// }
+}   
 
 export const createProductInward=(productInward)=>async (dispatch,getState)=>{
     try {
